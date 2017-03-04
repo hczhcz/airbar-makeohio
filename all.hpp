49629bb9ffa0c59c479099623867e54da21ecc2c
hcz <h.cz@qq.com>
2017-03-04 18:49:07 -0500

    init

---
+++ all.hpp
@@ -0,0 +1,37 @@
+#include <cstdlib>
+#include <cstdint>
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+
+#include <map>
+
+// 0 <= pitch <= 24
+// 0 <= volume <= 1
+// -1 <= vibe <= 1
+
+enum class Flag {
+
+};
+
+struct Note {
+    double pitch;
+    double volume;
+};
+
+std::map<uint64_t, Note> &fetch_notes();
+double fetch_vibe();
+bool fetch_flag(Flag c);
+
+std::map<uint64_t, Note> &smooth_notes(
+    const std::map<uint64_t, Note> &notes,
+    uint64_t time_div, double p, double up, double down
+);
+double smooth_vibe(
+    double vibe,
+    uint64_t time_div, double up, double down
+);
+
+std::map<uint64_t, Note> &apply_vibe(
+    const std::map<uint64_t, Note> &notes, double vibe
+);
