971d939a99be6d5b672b5cb14c87d6a3c62e1674
hcz <h.cz@qq.com>
2017-03-04 19:34:35 -0500

    working

--- main.cpp
+++ main.cpp
@@ -1,31 +1,90 @@
 #include "all.hpp"
 
-// tmp
-#include <unistd.h>
-#include <iostream>
-#define TIME_DIV 100
+#include "stk/RtAudio.h"
 
-int main() {
-    while (true) {
-        const std::map<uint64_t, Note> &data {
+// loop instruments
+#include "stk/Flute.h"
+#include "stk/Clarinet.h"
+#include "stk/Saxofony.h"
+#include "stk/Bowed.h"
+// non-loop instruments
+#include "stk/Plucked.h"
+#include "stk/Mandolin.h"
+#include "stk/Rhodey.h"
+
+int tick(
+    void *buffer, void *, unsigned int buffer_size, double,
+    RtAudioStreamStatus, void *
+) {
+    static std::map<uint64_t, stk::Instrmnt *> instruments;
+
+    stk::StkFloat *samples = (stk::StkFloat *) buffer;
+
+    for (unsigned int i = 0; i < buffer_size; ++i, ++samples) {
+        const std::map<uint64_t, Note> &notes {
             apply_vibe(
                 smooth_notes(
                     fetch_notes(),
-                    TIME_DIV, 5, 10, 5
+                    1000 / 44100. * buffer_size, 5, 10, 5 // TODO: use fp timestamp?
                 ),
                 smooth_vibe(
                     fetch_vibe(),
-                    TIME_DIV, 5, 5
+                    1000 / 44100. * buffer_size, 5, 5
                 )
             )
         };
 
-        for (auto i: data) {
-            std::cerr << i.second.pitch << " " << i.second.volume << "\t";
+        for (const auto &note: notes) {
+            if (instruments.find(note.first) == instruments.end()) {
+                instruments.insert({note.first, {
+                    new stk::Flute(0)
+                }});
+            }
         }
-        std::cerr << std::endl;
 
+        *samples = 0;
 
-        usleep(1000 * TIME_DIV);
+        for (auto &instrument: instruments) {
+            if (notes.find(instrument.first) == notes.end()) {
+                instrument.second->noteOff(1);
+                delete instrument.second;
+
+                instruments.erase(instrument.first);
+            } else {
+                const auto &note = notes.at(instrument.first);
+
+                instrument.second->setFrequency(440 * pow(2, note.pitch / 12));
+
+                *samples += note.volume
+                    * instrument.second->tick();
+            }
+        }
+    }
+
+    return 0;
+}
+
+int main() {
+    stk::Stk::setSampleRate(44100.);
+
+    RtAudio out;
+    RtAudio::StreamParameters parameters;
+
+    parameters.deviceId = out.getDefaultOutputDevice();
+    parameters.nChannels = 1;
+
+    RtAudioFormat format = sizeof(stk::StkFloat) == 8
+        ? RTAUDIO_FLOAT64
+        : RTAUDIO_FLOAT32;
+    unsigned int buffer_size = stk::RT_BUFFER_SIZE;
+
+    out.openStream(
+        &parameters, nullptr, format, (unsigned int) stk::Stk::sampleRate(),
+        &buffer_size, &tick, nullptr
+    );
+    out.startStream();
+
+    while (true) {
+        stk::Stk::sleep(1000);
     }
 }
