fd29a4e5d5e5d6e00191e8c50bc7ba37a40ad7c7
hcz <h.cz@qq.com>
2017-03-05 14:10:54 -0500

    implement vibe

--- fetch_vibe_gyroscope.cpp
+++ fetch_vibe_gyroscope.cpp
@@ -24,20 +24,31 @@ static int get_device() {
 }
 
 double fetch_vibe() {
+    static result = 0;
+
     static int device = -1;
 
-    //if (device == -1) {
-    //    device = get_device();
-    //    usleep(10 * 1000);
-    //}
+    if (device == -1) {
+       device = get_device();
+       usleep(10 * 1000);
+    }
 
     if (device != -1) {
+        double sum = 0;
+        size_t count = 0;
+
         int16_t action;
 
         while (read(device, &action, sizeof(action)) == sizeof(action)) {
-            fprintf(stderr, "%d", action);
+            sum += action / 32768.;
+            ++count;
         }
+
+        result = sum / (count + 0.001);
+        result *= std::abs(result);
+    } else {
+        result = 0;
     }
 
-    return 0; // TODO: mock
+    return 0;
 }
